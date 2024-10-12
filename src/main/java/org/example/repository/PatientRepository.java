20c26e1adced250a919e9da7cf1ff657737702ce
Bong Chong Hiang <96806368+bong033@users.noreply.github.com>
2024-10-12 08:43:08 +0800

    Add files via upload

--- src/main/java/org/example/repository/PatientRepository.java
+++ src/main/java/org/example/repository/PatientRepository.java
@@ -1,5 +1,6 @@
 package org.example.repository;
 
+import org.example.model.Package;
 import org.example.model.Patient;
 import org.example.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +9,8 @@ import java.util.Optional;
 
 public interface PatientRepository extends JpaRepository<Patient,Long> {
 
+    Optional<Patient> findByUser_UserId(Long userId);
+//    Optional<Patient> findPatientByUserId(Long userId);
     //Data Persistence: CrudRepository (Basic), JpaRepository (Advanced, extends from CrudRepository)
     //save()
     //findOne()
